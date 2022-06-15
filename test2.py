42589d600903429d6ee3ad492f9d4425b45f17e7
tranqkhue <khue.tranquoc@gmail.com>
2022-06-15 17:12:03 +0700

    Backup vERy ImP0rTAnT scipy lib somehow works

--- test2.py
+++ test2.py
@@ -16,7 +16,7 @@ import rospy
 import ros_numpy # Convert ROS messages into Numpy array
 from sensor_msgs.msg import PointCloud2
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 # Dimensions: first channel is lidar firing, second channel is physical axes!
 
@@ -46,7 +46,7 @@ def callback_ptcloud(ptcloud_data):
     pts  = np.moveaxis(pts, 0, 2)
     
     # Filter (smoothing) z value by column (or multiple ring in the same azimuth)
-    pts[:, :, 2] = savgol_filter(pts[:, :, 2], window_length=5, polyorder=3, axis=1)
+    # pts[:, :, 2] = savgol_filter(pts[:, :, 2], window_length=5, polyorder=3, axis=1)
     
     z0 = pts[:, 0:31, 2]
     z1 = pts[:, 1:32, 2]
@@ -61,17 +61,22 @@ def callback_ptcloud(ptcloud_data):
     # plt.hist(degrees_angle)
     # plt.show()
 
-    ANGLE_RANGE = 120
-    mask_2d = np.bitwise_and(degrees_angle>(90-ANGLE_RANGE/2), \
-                             degrees_angle<(90+ANGLE_RANGE/2))
-
+    OBSTACLE_SLOPE_ANGLE_RANGE = 120 # degree
+    mask_2d = np.bitwise_and(degrees_angle>(90-OBSTACLE_SLOPE_ANGLE_RANGE/2), \
+                             degrees_angle<(90+OBSTACLE_SLOPE_ANGLE_RANGE/2))
     mask_3d = np.repeat(mask_2d.reshape(-1, 31, 1), 3, axis=2)
-    ground_removed = np.ma.where(mask_3d, pts[:, 1:32, :], np.nan)
 
+    # Ground removed
+    ground_removed = np.ma.where(mask_3d, pts[:, 1:32, :], np.nan)
     # intensity = data['intensity'][:, 1:32]
     intensity = degrees_angle
     msg = pts_np_to_pclmsg(ground_removed, intensity=intensity)
-    gnd_publisher.publish(msg)
+    ground_removed_publisher.publish(msg)
+
+    # Ground seperated
+    ground = np.ma.where(np.bitwise_not(mask_3d), pts[:, 1:32, :], np.nan)
+    msg = pts_np_to_pclmsg(ground, intensity=intensity)
+    ground_seperated_publisher.publish(msg)
 
 #------------------------------------------------------------------------------
 
@@ -80,8 +85,10 @@ if (__name__ == "__main__"):
         rospy.init_node("test_lidar", anonymous=True)
         rospy.Subscriber("/velodyne_points", PointCloud2, callback_ptcloud, \
                          queue_size=1)
-        gnd_publisher = rospy.Publisher('/gnd_removed', PointCloud2,\
+        ground_removed_publisher = rospy.Publisher('/pointcloud/ground_removed', PointCloud2,\
                                         queue_size=1)
+        ground_seperated_publisher = rospy.Publisher('/pointcloud/ground_seperated', PointCloud2, \
+                                           queue_size=1)
         rospy.spin()
     except KeyboardInterrupt:
         cv2.destroyAllWindows()
