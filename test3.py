77052c060440dc5988a44be7144a9356285fca7f
tranqkhue <khue.tranquoc@gmail.com>
2022-06-17 00:09:00 +0700

    Modify test2 to absolute the angles. Add test3 for dangerzone

--- test3.py
+++ test3.py
@@ -8,6 +8,7 @@ Created on Fri May 27 00:56:26 2022
 
 # Fixing the OpenBLAS threading issue
 import os
+from cv2 import THRESH_OTSU
 os.environ["OPENBLAS_NUM_THREADS"] = "1" 
 
 #!/usr/bin/env python
@@ -65,7 +66,7 @@ def callback_ptcloud(ptcloud_data):
     r0 = r[:, 0:31]
     r1 = r[:, 1:32]
 
-    angle = np.abs(np.arctan2((z1-z0),(r1-r0)))
+    angle = np.arctan2((z1-z0),(r1-r0))
 
     # FOR TESTING ONLY! MAY CONFLICT WITH ROS CALLBACK
     # plt.hist(degrees_angle)
@@ -83,7 +84,27 @@ def callback_ptcloud(ptcloud_data):
 
     # Ground seperated
     ground = np.ma.where(np.bitwise_not(mask_3d), pts[:, 1:32, :], np.nan)
-    msg = pts_np_to_pclmsg(ground, intensity=intensity)
+    r = np.ma.where(np.bitwise_not(mask_3d), r[:, 1:32, :], np.nan)
+    
+    # --------------------------------
+    # tEsTiNG z0nE
+
+    # test_intensity = np.gradient(ground[:,:,2], axis=0) # Intra-ring
+    pts[:, :, 2] = savgol_filter(pts[:, :, 2], window_length=7, polyorder=5, axis=0)
+    
+    z0 = pts[1:, :, 2]
+    z1 = pts[:-1, :, 2]
+
+    r  = np.linalg.norm(pts[:, :, 0:2], axis=1)
+    r0 = r[1:, :]
+    r1 = r[:,-1, :]
+
+    angle = np.arctan2((z1-z0),(r1-r0))
+    np.clip(angle, a_min=0.01, a_max=0.5)
+    test_intensity = angle
+    # --------------------------------
+
+    msg = pts_np_to_pclmsg(ground, intensity=test_intensity)
     ground_seperated_publisher.publish(msg)
 
 #------------------------------------------------------------------------------
